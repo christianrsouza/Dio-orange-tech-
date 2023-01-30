37eb95d08e3d060a47e18a3261480594cd889035
Christian Souza <christianrsouza@hotmail.com>
2023-01-30 20:31:09 -0300

    Atualizao de aula

--- Java/meu-primeiro-projeto/src/MinhaClasse.java
+++ Java/meu-primeiro-projeto/src/MinhaClasse.java
@@ -1,5 +1,5 @@
-public class App {
+public class MinhaClasse {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.print("Olá mundo");
     }
 }
